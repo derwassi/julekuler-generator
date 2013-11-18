5f2459cc1dbb349c8329db3fc1fcd123fd0a44ad
wassi <der.wassi@web.de>
2013-11-18 20:07:55 +0100

    Better but still simple compression. 2 fields per character, use json only for colors. created map for encoding and decoding

--- js/julekuler.js
+++ js/julekuler.js
@@ -21,6 +21,137 @@ var colors = {
 	4 : '#26436E',
 	5: '#021530'
 };
+var encodeMap = {0:"-",
+1:".",
+2:"/",
+3:"0",
+4:"1",
+5:"2",
+6:"3",
+7:"4",
+8:"5",
+9:"6",
+10:"7",
+11:"8",
+12:"9",
+13:"B",
+14:"C",
+15:"D",
+16:"E",
+17:"F",
+18:"G",
+19:"H",
+20:"I",
+21:"J",
+22:"K",
+23:"L",
+24:"M",
+25:"N",
+26:"O",
+27:"P",
+28:"Q",
+29:"R",
+30:"S",
+31:"T",
+32:"U",
+33:"V",
+34:"W",
+35:"X",
+36:"Y",
+37:"Z",
+38:"a",
+39:"b",
+40:"c",
+41:"d",
+42:"e",
+43:"f",
+44:"g",
+45:"h",
+46:"i",
+47:"j",
+48:"k",
+49:"l",
+50:"m",
+51:"n",
+52:"o",
+53:"p",
+54:"q",
+55:"r",
+56:"s",
+57:"t",
+58:"u",
+59:"v",
+60:"w",
+61:"x",
+62:"y",
+63:"z"
+} 
+var decodeMap = {"-":0,
+".":1,
+"/":2,
+"0":3,
+"1":4,
+"2":5,
+"3":6,
+"4":7,
+"5":8,
+"6":9,
+"7":10,
+"8":11,
+"9":12,
+"B":13,
+"C":14,
+"D":15,
+"E":16,
+"F":17,
+"G":18,
+"H":19,
+"I":20,
+"J":21,
+"K":22,
+"L":23,
+"M":24,
+"N":25,
+"O":26,
+"P":27,
+"Q":28,
+"R":29,
+"S":30,
+"T":31,
+"U":32,
+"V":33,
+"W":34,
+"X":35,
+"Y":36,
+"Z":37,
+"a":38,
+"b":39,
+"c":40,
+"d":41,
+"e":42,
+"f":43,
+"g":44,
+"h":45,
+"i":46,
+"j":47,
+"k":48,
+"l":49,
+"m":50,
+"n":51,
+"o":52,
+"p":53,
+"q":54,
+"r":55,
+"s":56,
+"t":57,
+"u":58,
+"v":59,
+"w":60,
+"x":61,
+"y":62,
+"z":63
+}
+
 
 var renderer;
 var scene;
@@ -307,16 +438,53 @@ var createColorPicker=function(colors){
 };
 
 var loadPattern = function(patternstring){
+	try{
 	var obj = JSON.parse(patternstring);
-	colors = obj.colors;
-	createColorPicker(colors);
-	traverseDrawingSurface(16,13,function(row,col){
-			var $el = $('#pixel-'+row+'-'+col);
-			$el.attr('data-color',obj.pattern[row][col]);
-			$el.css('background-color',colors[obj.pattern[row][col]]);
+	}catch(e){}
+	if(obj){
+		colors = obj.colors;
+		createColorPicker(colors);
+		traverseDrawingSurface(16,13,function(row,col){
+				var $el = $('#pixel-'+row+'-'+col);
+				$el.attr('data-color',obj.pattern[row][col]);
+				$el.css('background-color',colors[obj.pattern[row][col]]);
 		
 		
-	});
+		});
+	}else{
+		patternstring = patternstring.split(';');
+		
+		for(var index in patternstring){
+			var parameter = patternstring[index].split('=');
+			//console.log(parameter);
+			if(parameter.length==2){
+				if(parameter[0]=='colors'){
+					colors = JSON.parse(parameter[1]);				
+				}			
+				if(parameter[0]=='data'){
+					var data = parameter[1];
+					console.log(data);
+					var i=0;
+					var bin=0;
+					traverseDrawingSurface(16,13,function(row,col){
+							var $el = $('#pixel-'+row+'-'+col);
+	
+							var bin = decode6BitToChar(data.charAt(Math.floor(i/2)));		
+							if(i%2==1){
+								bin = bin>>3//last three bit
+							}else{
+								bin = bin&7;//first three bit
+							}
+							i++;
+							$el.attr('data-color',bin);
+							$el.css('background-color',colors[bin]);
+							
+						
+					});
+				}
+			}
+		}
+	}
 	redraw();
 	texture1.needsUpdate = true;
 	
@@ -342,6 +510,14 @@ var createBookmarkLink=function() {
     }
     return false;
 }
+
+var encode6BitToChar = function(bin){
+	return encodeMap[bin];
+}
+var decode6BitToChar = function(bin){
+	return decodeMap[bin];
+}
+
 
 var addEvents = function(){
 	var centerWidth=16;
@@ -380,17 +556,30 @@ var addEvents = function(){
 	});
 	$("#save").click(function(){
 		var s = {};
+		var i=0;
+		var bin = 0;
+		var delta = 42;//'0' char
+		var res = '';
 		traverseDrawingSurface(16,13,function(row,col){
+			//convert to string, 6 colors = 3 bit => 6 bit for 2 fields => fits into 64 characters (a-zA-Z0-9./)
 			var val = parseInt($('#pixel-'+row+'-'+col).attr('data-color'));
 			if(!val) val=0;
 			if(!s[row]) s[row] = {};
-			
 			s[row][col]=val;
+			bin|=val<<(3*(i%2));
+			if(i%2==1){
+				res+=encode6BitToChar(bin);
+				bin=0;
+			}
+			i++;
+			
+			
 		});
+		console.log(res);
 		var json = JSON.stringify({colors:colors,pattern:s});
 		$('#load-save').val(json);	
 		document.title = $('#title').val() + " - Julekuler generator";
-		window.location.hash = '#' + encodeURIComponent(json);
+		window.location.hash = '#colors='+JSON.stringify(colors)+';data=' + res;
 		return createBookmarkLink();
 
 	});
